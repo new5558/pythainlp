77d1484c20dfb497fd29725bfbe2bd23fca4626a
Arthit Suriyawongkul <arthit@gmail.com>
2024-11-01 17:05:39 +0000

    Remove is_native_thai test

--- tests/test_util.py
+++ tests/test_util.py
@@ -29,7 +29,6 @@ from pythainlp.util import (
     expand_maiyamok,
     find_keyword,
     ipa_to_rtgs,
-    is_native_thai,
     isthai,
     isthaichar,
     nectec_to_ipa,
@@ -616,27 +615,6 @@ class UtilTestCase(unittest.TestCase):
         self.assertEqual(isthai("(ต.ค.)"), False)
         self.assertEqual(isthai("ต.ค.", ignore_chars=None), False)
         self.assertEqual(isthai("(ต.ค.)", ignore_chars=".()"), True)
-
-    def test_is_native_thai(self):
-        self.assertEqual(is_native_thai(None), False)
-        self.assertEqual(is_native_thai(""), False)
-        self.assertEqual(is_native_thai("116"), False)
-        self.assertEqual(is_native_thai("abc"), False)
-        self.assertEqual(is_native_thai("ตา"), True)
-        self.assertEqual(is_native_thai("ยา"), True)
-        self.assertEqual(is_native_thai("ฆ่า"), True)
-        self.assertEqual(is_native_thai("คน"), True)
-        self.assertEqual(is_native_thai("กะ"), True)
-        self.assertEqual(is_native_thai("มอ"), True)
-        self.assertEqual(is_native_thai("กะ"), True)
-        self.assertEqual(is_native_thai("กระ"), True)
-        self.assertEqual(is_native_thai("ประท้วง"), True)
-        self.assertEqual(is_native_thai("ศา"), False)
-        self.assertEqual(is_native_thai("ลักษ์"), False)
-        self.assertEqual(is_native_thai("มาร์ค"), False)
-        self.assertEqual(is_native_thai("เลข"), False)
-        self.assertEqual(is_native_thai("เทเวศน์"), False)
-        self.assertEqual(is_native_thai("เทเวศร์"), False)
 
     def test_display_thai_char(self):
         self.assertEqual(display_thai_char("้"), "_้")
