091a6750f482b5da3f111a59e6d84678674c40d1
Arthit Suriyawongkul <arthit@gmail.com>
2024-11-01 12:34:31 +0000

    Use common warn_deprecation

---
+++ pythainlp/tools/core.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2016-2024 PyThaiNLP Project
+# SPDX-License-Identifier: Apache-2.0
+"""
+Generic support functions for PyThaiNLP.
+"""
+
+import warnings
+
+
+def warn_deprecation(
+    deprecated_func: str,
+    replacing_func: str = "",
+    version: str = "",
+):
+    """
+    Warn about the deprecation of a function.
+
+    :param str deprecated_func: Name of the deprecated function.
+    :param str replacing_func: Name of the function to use instead (optional).
+    :param str version: PyThaiNLP version in which the function will be deprecated (optional).
+    """
+    if version:
+        version = f"PyThaiNLP {version}"
+    else:
+        version = "a future release"
+    message = f"The '{deprecated_func}' function is deprecated and will be removed in {version}."
+    if replacing_func:
+        message += f" Please use '{replacing_func}' instead."
+    warnings.warn(message, DeprecationWarning, stacklevel=2)
--- pythainlp/util/thaiwordcheck.py
+++ pythainlp/util/thaiwordcheck.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
 # SPDX-FileCopyrightText: 2016-2024 PyThaiNLP Project
 # SPDX-License-Identifier: Apache-2.0
-import warnings
+from pythainlp.tools import warn_deprecation
 
 
 def is_native_thai(word: str) -> bool:
-    warnings.warn(
-        """
-        pythainlp.util.is_native_thai has been renamed to \
-            pythainlp.morpheme.is_native_thai.
-        This function will be removed in PyThaiNLP 5.1.
-        """,
-        DeprecationWarning,
+    warn_deprecation(
+        "pythainlp.util.is_native_thai",
+        "pythainlp.morpheme.is_native_thai",
+        "5.1",
     )
+
     from pythainlp.morpheme import is_native_thai as check
 
     return check(word)
