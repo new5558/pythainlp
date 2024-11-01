13f80477701625e439c58c9e6e4e094c35a26a1c
Arthit Suriyawongkul <arthit@gmail.com>
2024-11-01 10:03:58 +0000

    Add notes on load test protocol

--- tests/__init__.py
+++ tests/__init__.py
@@ -9,6 +9,7 @@ Each file in tests/ is for each main package.
 
 from unittest import TestLoader, TestSuite
 
+# Names of module to be tested
 test_packages: list[str] = [
     "tests.test_ancient",
     # "tests.test_cli",
@@ -24,13 +25,17 @@ test_packages: list[str] = [
 def load_tests(
     loader: TestLoader, standard_tests: TestSuite, pattern: str
 ) -> TestSuite:
-    """Load test protocol"""
+    """Load test protocol
+    See: https://docs.python.org/3/library/unittest.html#id1
+    """
     suite = TestSuite()
     for test_package in test_packages:
         tests = loader.loadTestsFromName(test_package)
         suite.addTests(tests)
     return suite
 
+
 if __name__ == "__main__":
     import unittest
+
     unittest.main()
