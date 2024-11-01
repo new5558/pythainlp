205f05f0b61f1f284880aac61ede2ddc1cfd3802
Arthit Suriyawongkul <arthit@gmail.com>
2024-11-01 08:03:09 +0000

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -10,22 +10,23 @@ Each file in tests/ is for each main package.
 from unittest import TestLoader, TestSuite
 
 test_packages: list[str] = [
-    "tests.test_ancient",
+    # "tests.test_ancient",
     # "tests.test_cli",
     # "tests.test_corpus",
-    # "tests.test_tokenize",
-    # "tests.test_transliterate",
-    # "tests.test_spell",
     # "tests.test_soundex",
+    # "tests.test_spell",
+    "tests.test_tokenize",
+    # "tests.test_transliterate",
     # "tests.test_util",
 ]
 
-from . import test_tokenize
 
-
-def load_tests(loader: TestLoader, tests, pattern) -> TestSuite:
-    """A function to load tests."""
+def load_tests(
+    loader: TestLoader, standard_tests: TestSuite, pattern: str
+) -> TestSuite:
+    """Load test protocol"""
     suite = TestSuite()
-    tests = loader.loadTestsFromModule(test_tokenize)
-    suite.addTests(tests)
+    for test_package in test_packages:
+        tests = loader.loadTestsFromName(test_package)
+        suite.addTests(tests)
     return suite
