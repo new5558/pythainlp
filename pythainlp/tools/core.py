d4405e1a30e91b47a769d7fa8f1a294860c62875
Arthit Suriyawongkul <arthit@gmail.com>
2024-11-02 08:33:07 +0000

    Make it print safely on Windows

--- pythainlp/tools/core.py
+++ pythainlp/tools/core.py
@@ -5,6 +5,7 @@
 Generic support functions for PyThaiNLP.
 """
 
+import sys
 import warnings
 
 
@@ -13,8 +14,7 @@ def warn_deprecation(
     replacing_func: str = "",
     version: str = "",
 ):
-    """
-    Warn about the deprecation of a function.
+    """Warn about the deprecation of a function.
 
     :param str deprecated_func: Name of the deprecated function.
     :param str replacing_func: Name of the function to use instead (optional).
@@ -28,3 +28,19 @@ def warn_deprecation(
     if replacing_func:
         message += f" Please use '{replacing_func}' instead."
     warnings.warn(message, DeprecationWarning, stacklevel=2)
+
+
+def safe_print(text: str):
+    """Print text to console, handling UnicodeEncodeError.
+
+    :param text: Text to print.
+    :type text: str
+    """
+    try:
+        print(text)
+    except UnicodeEncodeError:
+        print(
+            text.encode(sys.stdout.encoding, errors="replace").decode(
+                sys.stdout.encoding
+            )
+        )
