2ae9ba622866939b8cbb33c63df59d66f102d543
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2025-01-10 22:02:36 +0700

    Update longest.py

--- pythainlp/tokenize/longest.py
+++ pythainlp/tokenize/longest.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # SPDX-FileCopyrightText: 2016-2025 PyThaiNLP Project
 # SPDX-FileType: SOURCE
-# SPDX-FileType: SOURCE
 # SPDX-License-Identifier: Apache-2.0
 """
 Dictionary-based longest-matching Thai word segmentation. Implementation is based
